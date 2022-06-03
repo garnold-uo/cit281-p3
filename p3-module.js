fe56fefc44a1587f62337a4722725ff45b2b181b
garnold-uo <105458607+garnold-uo@users.noreply.github.com>
2022-06-03 12:41:11 -0700

    Add files via upload

---
+++ p3-module.js
@@ -0,0 +1,41 @@
+function validDenomination(coin) {
+    // Test if coin = 1, 5, 10, 25, 50, 100
+    return [1, 5, 10, 25, 50, 100].indexOf(coin) !== -1;
+}
+
+function valueFromCoinObject(obj) {
+    // Extract denom and count from obj
+    // Set default values of denom and count to 0
+    // Calculate value of coin object
+
+    const { denom = 0, count = 0, } = obj;
+    return denom * count;
+}
+
+function valueFromArray(arr) {
+    // When arr contains inner array
+    if (Array.isArray(arr[0])) {
+        arr = arr[0];
+    }
+
+    // Sum of all values in arr
+    return arr.reduce((accumulator, currentObj) =>
+        accumulator + valueFromCoinObject(currentObj), 0);
+}
+
+function coinCount(...coinage) {
+    // Call and return valueFromArray() with coinage as paramater 
+    return valueFromArray(coinage)
+}
+
+module.exports = {
+    // Export coinCount
+    coinCount
+};
+
+
+console.log("{}", coinCount({denom: 5, count: 3}));
+console.log("{}s", coinCount({denom: 5, count: 3},{denom: 10, count: 2}));
+const coins = [{denom: 25, count: 2},{denom: 1, count: 7}];
+console.log("...[{}]", coinCount(...coins));
+console.log("[{}]", coinCount(coins));
---
+++ p3-server.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+const coinCount = require('./p3-module'); 
+const fastify = require("fastify")();
+
+fastify.get("/", (request, reply) => {
+    fs.readFile(`${__dirname}/index.html`, (err, data) => {
+        if (err) {
+            reply.code = 500;
+            console.log(err);
+            reply.header("Content-Type", "text/html; charset=utf-8");
+        } 
+        else {
+            reply.code = 200;
+            reply.header("Content-Type", "text/html; charset=utf-8");
+            }
+        });
+});
+
+fastify.get("/coin", (request, reply) => {
+    const { denom = 0, count = 0 } = request.query;
+        if (err) {
+            reply.code = 500;
+            console.log(err);
+            reply.header("Content-Type", "text/html; charset=utf-8");
+        } 
+        else {
+            reply.code = 200;
+            reply.header("Content-Type", "text/html; charset=utf-8");
+            reply.send(`<h2>Value of ${count} of ${denom} is ${coinValue}</h2><br /><a href="/">Home</a>`)
+            }
+});
+
+fastify.get("/coins", (request, reply) => {
+    const { option } = request.query;
+        if (err) {
+            reply.code = 500;
+            console.log(err);
+            reply.header("Content-Type", "text/html; charset=utf-8");
+        } 
+        else {
+            reply.code = 200;
+            reply.header("Content-Type", "text/html; charset=utf-8");
+            reply.send(`<h2>Value of ${count} of ${denom} is ${coinValue}</h2><br /><a href="/">Home</a>`)
+            coinCount({ denom: 5, count: 3 }, { denom: 10, count: 2 });
+            coinCount(...coins);
+            coinCount(coins); 
+            }
+});
+
+const listenIP = "localhost";
+const listenPort = 8080;
+
+fastify.listen(listenPort, listenIP, (err, address) => {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+  console.log(`Server listening on ${address}`);
+});
